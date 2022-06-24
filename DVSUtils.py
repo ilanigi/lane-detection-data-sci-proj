8c584bca2ac7323107c785c2ec5c3ac43433a1f1
ilanigi <gilad.ilani@gmail.com>
2022-06-24 17:02:18 +0300

    now it works

--- DVSUtils.py
+++ DVSUtils.py
@@ -1,6 +1,5 @@
 import numpy as np
-import cv2
-from cv2.cv2 import HoughLines
+from skimage.transform import hough_line, hough_line_peaks
 from matplotlib import pyplot as plt
 
 
@@ -16,37 +15,54 @@ def plot_by_points(img):
 
 def hough_line_transform(img):
     img = delete_none_binary_pixels(img)
-    img = delete_noise_by_neighbors(img, min_neighbors_amount=2)
-    img = delete_noise_by_neighbors(img, min_neighbors_amount=1)
-    # img = cv2.GaussianBlur(img, (5, 5), cv2.BORDER_DEFAULT)
+    img, counter = delete_noise_by_neighbors(img, min_neighbors_amount=2)
 
     img_unit8 = np.uint8(img)
-    lines = HoughLines(img_unit8, 1, np.pi / 180, 20)
-    for rho, theta in lines[0]:
-        a = np.cos(theta)
-        b = np.sin(theta)
-        x0 = a * rho
-        y0 = b * rho
-        x1 = int(x0 + 1000 * (-b))
-        y1 = int(y0 + 1000 * (a))
-        x2 = int(x0 - 1000 * (-b))
-        y2 = int(y0 - 1000 * (a))
+    tested_angles = np.linspace(-np.pi / 2, np.pi / 2, 180)
+    hspace, theta, dist = hough_line(img_unit8, tested_angles)
+    angle_list = []
+    fig, axes = plt.subplots(1, 1)
 
-        cv2.line(img, (x1, y1), (x2, y2), (255, 50, 20), 2)
-        cv2.imshow('1', img)
 
-        cv2.waitKey(0)
-        cv2.destroyAllWindows()
+
+    axes.imshow(np.log(1 + hspace), aspect=0.05)
+    plt.show()
+    fig, axes = plt.subplots(1, 1)
+    axes.imshow(img, cmap='gray')
+
+    origin = np.array((0, img.shape[1]))
+    min_angels = [(9999,0), (-9999,0)]
+    for _, angle, dist in zip(*hough_line_peaks(hspace, theta, dist)):
+        if abs(angle) < 0.1:
+            continue
+
+        if 0 < angle < min_angels[0][0]:
+            min_angels[0] = (angle, dist)
+        elif min_angels[1][0] < angle < 0 and abs(abs(min_angels[0][0]) - abs(angle)) > 0.1:
+            min_angels[1] = (angle, dist)
+
+    for angle, dist in min_angels:
+        angle_list.append(angle)
+        y0, y1 = (dist - origin * np.cos(angle)) / np.sin(angle)
+        axes.plot(origin, (y0, y1), '-r')
+    axes.set_xlim(origin)
+    axes.set_ylim((img.shape[0], 0))
+    axes.set_axis_off()
+    axes.set_title('Detected lines')
+
+    plt.show()
 
 
 def delete_noise_by_neighbors(img, neighbor=[-1, 0, 1], min_neighbors_amount=3):
     height, width = img.shape
+    total_counter = 0
     out_img = np.zeros((height, width))
     for i in range(height):
         for j in range(width):
             if img[i, j] == 0:
                 continue
             counter = 0
+            total_counter += 1
             for k in neighbor:
                 for l in neighbor:
                     try:
@@ -58,7 +74,7 @@ def delete_noise_by_neighbors(img, neighbor=[-1, 0, 1], min_neighbors_amount=3):
             if counter - 1 > min_neighbors_amount:
                 out_img[i, j] = 255
 
-    return out_img
+    return out_img, total_counter
 
 
 def delete_none_binary_pixels(img):
