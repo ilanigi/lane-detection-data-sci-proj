5e05cd84aa141692b1ceae49674ac2d8481a4d65
ilanigi <61771415+ilanigi@users.noreply.github.com>
2022-06-24 17:11:41 +0300

    Hough trsform (#5)
    * working-ish
    
    * pass by error
    
    * now it works
    
    * deleted counter
    
    * changed var names

--- DVS.py
+++ DVS.py
@@ -1,14 +1,12 @@
 import cv2
-from DVSUtils import get_data_from_image, delete_none_binary_pixels, delete_noise_by_neighbors, plot_by_points
+from DVSUtils import get_data_from_image, delete_none_binary_pixels, delete_noise_by_neighbors, plot_by_points, \
+    hough_line_transform
 
 img1 = cv2.imread('10.jpg')
-
 img1 = cv2.cvtColor(img1, cv2.COLOR_BGR2GRAY)
-height, width = img1.shape
-img1 = delete_none_binary_pixels(img1)
-img1 = delete_noise_by_neighbors(img1,min_neighbors_amount=2)
-img1 = delete_noise_by_neighbors(img1,min_neighbors_amount=1)
-plot_by_points(img1)
+
+hough_line_transform(img1)
+# delete_noise_by_neighbors(img1)
 # img1 = set_triangle_scope(img1)
 # img1 = delete_right_half(img1)
 
--- DVSUtils.py
+++ DVSUtils.py
@@ -1,15 +1,57 @@
 import numpy as np
+from skimage.transform import hough_line, hough_line_peaks
 from matplotlib import pyplot as plt
 
 
 def plot_by_points(img):
+    img = delete_none_binary_pixels(img)
+    img = delete_noise_by_neighbors(img, min_neighbors_amount=2)
+    img = delete_noise_by_neighbors(img, min_neighbors_amount=1)
     points = get_data_from_image(img)
     y, x = zip(*points)
     plt.scatter(x, y, s=0.5, c='k')
     plt.show()
 
 
-def delete_noise_by_neighbors(img, neighbor=[-1, 0, 1], min_neighbors_amount=3):
+def hough_line_transform(img):
+    img = delete_none_binary_pixels(img)
+    img = delete_noise_by_neighbors(img, min_neighbors_amount=2)
+
+    img_unit8 = np.uint8(img)
+    tested_angles = np.linspace(-np.pi / 2, np.pi / 2, 180)
+    h_space, theta, dist = hough_line(img_unit8, tested_angles)
+    angle_list = []
+    fig, axes = plt.subplots(1, 1)
+
+    axes.imshow(np.log(1 + h_space), aspect=0.05)
+    plt.show()
+    fig, axes = plt.subplots(1, 1)
+    axes.imshow(img, cmap='gray')
+
+    origin = np.array((0, img.shape[1]))
+    min_angels = [(9999, 0), (-9999, 0)]
+    for _, angle, dist in zip(*hough_line_peaks(h_space, theta, dist)):
+        if abs(angle) < 0.1:
+            continue
+
+        if 0 < angle < min_angels[0][0]:
+            min_angels[0] = (angle, dist)
+        elif min_angels[1][0] < angle < 0 and abs(abs(min_angels[0][0]) - abs(angle)) > 0.1:
+            min_angels[1] = (angle, dist)
+
+    for angle, dist in min_angels:
+        angle_list.append(angle)
+        y0, y1 = (dist - origin * np.cos(angle)) / np.sin(angle)
+        axes.plot(origin, (y0, y1), '-r')
+    axes.set_xlim(origin)
+    axes.set_ylim((img.shape[0], 0))
+    axes.set_axis_off()
+    axes.set_title('Detected lines')
+
+    plt.show()
+
+
+def delete_noise_by_neighbors(img, kernel=[-1, 0, 1], min_neighbors_amount=3):
     height, width = img.shape
     out_img = np.zeros((height, width))
     for i in range(height):
@@ -17,12 +59,13 @@ def delete_noise_by_neighbors(img, neighbor=[-1, 0, 1], min_neighbors_amount=3):
             if img[i, j] == 0:
                 continue
             counter = 0
-            for k in neighbor:
-                for l in neighbor:
+            for k in kernel:
+                for m in kernel:
                     try:
-                        if img[i + k, j + l] == 255:
+                        if img[i + k, j + m] == 255:
                             counter += 1
-                    except:
+                    except IndexError:
+                        # kernel out of picture bound
                         pass
             # -1 ==> don't count a pixel as its own neighbor!
             if counter - 1 > min_neighbors_amount:
