1128643063db1453e1e9ff9c4602f4ad2d68e52e
ilanigi <gilad.ilani@gmail.com>
2022-06-25 12:52:43 +0300

    done

--- DVSUtils.py
+++ DVSUtils.py
@@ -1,6 +1,7 @@
 import numpy as np
-from skimage.transform import hough_line, hough_line_peaks
+from skimage.transform import hough_line, hough_line_peaks, hough_circle, hough_circle_peaks
 from matplotlib import pyplot as plt
+from skimage.transform._hough_transform import circle_perimeter
 
 
 def plot_by_points(img):
@@ -12,6 +13,60 @@ def plot_by_points(img):
     plt.scatter(x, y, s=0.5, c='k')
     plt.show()
 
+
+def all_hough_circle_transform(img):
+    img = delete_none_binary_pixels(img)
+    img = delete_noise_by_neighbors(img, min_neighbors_amount=2)
+
+    img_unit8 = np.uint8(img)
+    radius = np.arange(450, 500)
+    h_space = hough_circle(img_unit8, radius)
+    accum, cx, cy, rad = hough_circle_peaks(h_space, radius)
+
+    fig, ax = plt.subplots(ncols=1, nrows=1, figsize=(10, 4))
+    counter = 0
+    for center_y, center_x, radius in zip(cy, cx, rad):
+        if counter == 100:
+            break
+        counter += 1
+        circ_y, circ_x = circle_perimeter(center_y, center_x, radius, shape=img.shape)
+        img[circ_y, circ_x] = 225
+
+    ax.imshow(img, cmap=plt.cm.gray)
+    plt.show()
+
+
+
+
+
+
+
+def all_hough_lines_transform(img):
+    img = delete_none_binary_pixels(img)
+    img = delete_noise_by_neighbors(img, min_neighbors_amount=2)
+
+    img_unit8 = np.uint8(img)
+    tested_angles = np.linspace(-np.pi / 2, np.pi / 2, 180)
+    h_space, theta, dist = hough_line(img_unit8, tested_angles)
+    angle_list = []
+    fig, axes = plt.subplots(1, 1)
+
+    axes.imshow(np.log(1 + h_space), aspect=0.05)
+    plt.show()
+    fig, axes = plt.subplots(1, 1)
+    axes.imshow(img, cmap='gray')
+
+    origin = np.array((0, img.shape[1]))
+    for _, angle, dist in zip(*hough_line_peaks(h_space, theta, dist)):
+        angle_list.append(angle)
+        y0, y1 = (dist - origin * np.cos(angle)) / np.sin(angle)
+        axes.plot(origin, (y0, y1), '-r')
+    axes.set_xlim(origin)
+    axes.set_ylim((img.shape[0], 0))
+    axes.set_axis_off()
+    axes.set_title('Detected lines')
+
+    plt.show()
 
 def hough_line_transform(img):
     img = delete_none_binary_pixels(img)
@@ -46,7 +101,7 @@ def hough_line_transform(img):
     axes.set_xlim(origin)
     axes.set_ylim((img.shape[0], 0))
     axes.set_axis_off()
-    axes.set_title('Detected lines')
+    axes.set_title('Min detected lines')
 
     plt.show()
 
