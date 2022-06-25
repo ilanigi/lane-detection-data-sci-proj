44187d461efb63bbc7e71ea462cff6c08499fa4d
ilanigi <gilad.ilani@gmail.com>
2022-06-25 17:03:50 +0300

    ransac

--- DVS.py
+++ DVS.py
@@ -1,13 +1,13 @@
 import cv2
 from Utils.Crop import get_bottom_right_corner
 from Utils.Preprocess import general
-from Utils.Regressions import RANSAC
+from Utils.Regressions import plot_RANSAC, RANSAC
 
 
 def main():
     img1 = general('10.jpg')
     img1 = get_bottom_right_corner(img1)
-    RANSAC(img1)
+    plot_RANSAC(img1)
 
 
 if __name__ == '__main__':
--- Utils/Regressions.py
+++ Utils/Regressions.py
@@ -9,32 +9,41 @@ from Utils.Preprocess import image_to_data, plot_data, general
 def RANSAC(img):
     points = image_to_data(img)
     X, y = zip(*points)
-    plot_data(points)
+
     y = np.asarray(y)
     X = np.array([[point] for point in X])
 
-    lr = linear_model.LinearRegression()
-    lr.fit(X, y)
+    ransac = linear_model.RANSACRegressor()
+    ransac.fit(X, y)
+
+    line_X = np.arange(X.min(), X.max())[:, np.newaxis]
+    line_y_ransac = ransac.predict(line_X)
+    print(line_y_ransac)
+
+
+def plot_RANSAC(img):
+    points = image_to_data(img)
+    X, y = zip(*points)
+    y = np.asarray(y)
+    X = np.array([[point] for point in X])
 
     # Robustly fit linear model with RANSAC algorithm
     ransac = linear_model.RANSACRegressor()
     ransac.fit(X, y)
-    inlier_mask = ransac.inlier_mask_
-    outlier_mask = np.logical_not(inlier_mask)
+    inliner_mask = ransac.inlier_mask_
+    outliner_mask = np.logical_not(inliner_mask)
 
     # Predict data of estimated models
     line_X = np.arange(X.min(), X.max())[:, np.newaxis]
-    line_y = lr.predict(line_X)
     line_y_ransac = ransac.predict(line_X)
 
     lw = 2
     plt.scatter(
-        X[inlier_mask], y[inlier_mask], color="yellowgreen", marker=".", label="Inliers"
+        X[inliner_mask], y[inliner_mask], color="yellowgreen", marker=".", label="Inliers"
     )
     plt.scatter(
-        X[outlier_mask], y[outlier_mask], color="gold", marker=".", label="Outliers"
+        X[outliner_mask], y[outliner_mask], color="gold", marker=".", label="Outliers"
     )
-    plt.plot(line_X, line_y, color="navy", linewidth=lw, label="Linear regressor")
     plt.plot(
         line_X,
         line_y_ransac,
