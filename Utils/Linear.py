e4100a2c00b8ef3baa4f032f93b4ce02d3d7197c
ilanigi <61771415+ilanigi@users.noreply.github.com>
2022-06-25 13:04:12 +0300

    All hough (#6)
    * done
    
    * \n
    
    * split utils to submodules
    
    * changed import and dir name

---
+++ Utils/Linear.py
@@ -0,0 +1,22 @@
+def linear_equation(base_point, mid_point):
+    base_x, base_y = base_point
+    x_middle_point, y_middle_point, = mid_point
+    m = (y_middle_point - base_y) / (x_middle_point - base_x)
+    n = - (m * x_middle_point - y_middle_point)
+    return lambda x: m * x + n
+
+
+def set_triangle_scope(img, mid_point=(640, 200), base_height=650):
+    height, width = img.shape
+    left_linear_equation = linear_equation((0, base_height), mid_point)
+    right_linear_equation = linear_equation((width, base_height), mid_point)
+    for i in range(height):
+        for j in range(width):
+            if left_linear_equation(j) > i or right_linear_equation(j) > i:
+                img[i, j] = 0
+    return img
+
+
+def delete_right_half(img):
+    height, width = img.shape
+    return img[:, :int(width / 2)]
