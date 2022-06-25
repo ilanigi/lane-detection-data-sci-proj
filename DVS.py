48c05c98f605bb27c141321d0fc3b07bb504e7a4
ilanigi <gilad.ilani@gmail.com>
2022-06-25 13:40:39 +0300

    main

--- DVS.py
+++ DVS.py
@@ -2,7 +2,12 @@ import cv2
 from Utils.Crop import get_bottom_right_corner
 from Utils.Preprocess import plot_by_points, general
 
-img1 = general('10.jpg')
-img1 = get_bottom_right_corner(img1)
-img1 =
 
+def main():
+    img1 = general('10.jpg')
+    img1 = get_bottom_right_corner(img1)
+
+
+
+if __name__ == '__main__':
+    main()
