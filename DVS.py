ca365743f37cb45ad5c2526a46839341ca5f0f18
ilanigi <gilad.ilani@gmail.com>
2022-06-25 13:38:45 +0300

    crop

--- DVS.py
+++ DVS.py
@@ -1,10 +1,8 @@
 import cv2
-from Utils.Linear import get_bottom_right_corner
-from Utils.Preprocess import delete_none_binary_pixels, delete_noise_by_neighbors, get_data_from_image, plot_by_points, \
-    general
+from Utils.Crop import get_bottom_right_corner
+from Utils.Preprocess import plot_by_points, general
 
 img1 = general('10.jpg')
 img1 = get_bottom_right_corner(img1)
-# points = get_data_from_image(img1)
+img1 =
 
-plot_by_points(img1)
