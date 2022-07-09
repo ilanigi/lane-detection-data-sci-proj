e5bd1d2e8dade79b5225753212b7dc747a35bfe7
ilanigi <gilad.ilani@gmail.com>
2022-07-09 10:53:47 +0300

    added main_par_regression_loop

--- src/DVS.py
+++ src/DVS.py
@@ -1,11 +1,9 @@
-from Utils.Preprocess import general, get_data_from_parallelogram
-from src.Utils.Plot import draw_rectangle, show_image, get_rectangle_from_mid_bottom, draw_parallelogram, plot_data
-from src.Utils.Regressions import get_point_from_RANSAC, line_from_RANSAC, get_par_from_mid_point
+from Utils.Regressions import main_par_regression_loop
 
 
 def main():
 
-
+    main_par_regression_loop()
 
 if __name__ == '__main__':
     main()
