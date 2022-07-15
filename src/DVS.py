6ab30d6a54a4d4ba62ea0c51fb5d3337883ad753
ilanigi <gilad.ilani@gmail.com>
2022-07-15 16:13:34 +0300

    added points

--- src/DVS.py
+++ src/DVS.py
@@ -2,7 +2,8 @@ from Utils.Regressions import main_par_regression_loop
 
 
 def main():
-    main_par_regression_loop()
+    left_points, right_points = main_par_regression_loop()
+    
     
 if __name__ == '__main__':
     main()
